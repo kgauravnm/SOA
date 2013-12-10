b706578df33e32b677126c309009e1104d180402
Jakuub Uhrik <jakuub@mail.com>
2013-12-10 19:24:03 +0100

    removed copied customer

--- example-customer-service/src/main/java/com/example/customerservice/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.example.customerservice;
-
-import javax.jws.WebService;
-
-@WebService
-public interface CustomerService {
-	Customer getInformation(String id) throws NoSuchCustomerException;
-}
