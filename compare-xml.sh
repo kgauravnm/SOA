3fec664aa1c53b5c700399b730330426953522b1
kgauravnm <kgauravnm@gmail.com>
2025-02-12 20:15:41 +0530

    Update compare-xml.sh

--- compare-xml.sh
+++ compare-xml.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
-# Input file containing multiple XML documents
-input_file="multi_xml_file.xml"
+# Input files containing multiple XML documents
+file1="file1.xml"
+file2="file2.xml"
 
-# Temporary file to hold individual XML documents
-temp_file=$(mktemp)
+# Temporary directory to hold individual XML documents
+temp_dir=$(mktemp -d)
 
-# Counter to keep track of XML documents
-doc_count=0
+# Function to split a file into individual XML documents
+split_xml_file() {
+    local input_file=$1
+    local output_prefix=$2
 
-# Use awk to split the file into individual XML documents
-awk 'BEGIN { RS="</document>"; FS="\n" } {
-    if ($0 ~ /<document>/) {
-        doc_count++
-        print $0 "</document>" > "'"$temp_file"'_" doc_count ".xml"
-    }
-}' "$input_file"
+    # Use awk to split the file into individual XML documents
+    awk 'BEGIN { RS="</document>"; FS="\n" } {
+        if ($0 ~ /<document>/) {
+            doc_count++
+            print $0 "</document>" > "'"$temp_dir"'/'$output_prefix'_" doc_count ".xml"
+        }
+    }' "$input_file"
+}
+
+# Split the first file into individual XML documents
+split_xml_file "$file1" "file1"
+
+# Split the second file into individual XML documents
+split_xml_file "$file2" "file2"
 
 # Compare the XML documents
-for file in "${temp_file}_"*.xml; do
-    echo "Processing $file..."
-    # Use xmllint to format and validate the XML
-    xmllint --format "$file" --output "$file"
-    
-    # Example: Compare the current XML document with the first one
-    if [ "$file" != "${temp_file}_1.xml" ]; then
-        echo "Comparing ${temp_file}_1.xml with $file..."
-        diff "${temp_file}_1.xml" "$file"
+for file1_xml in "$temp_dir"/file1_*.xml; do
+    # Extract the document number from the filename
+    doc_number=$(basename "$file1_xml" | sed 's/file1_\(.*\).xml/\1/')
+    file2_xml="$temp_dir/file2_$doc_number.xml"
+
+    # Check if the corresponding XML document exists in the second file
+    if [ -f "$file2_xml" ]; then
+        echo "Comparing document $doc_number..."
+        diff "$file1_xml" "$file2_xml"
+    else
+        echo "Document $doc_number not found in $file2."
     fi
 done
 
 # Clean up temporary files
-rm "${temp_file}_"*.xml
+rm -r "$temp_dir"
